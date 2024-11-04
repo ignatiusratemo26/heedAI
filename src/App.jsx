8e09c1ed79acb0d5caa88da299adf0954d860f97
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-11-04 13:22:07 +0300

    defined the layout of the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
-
-function Home() {
-  return (
-    <div className='Home'>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import Home from './pages/Home';
 
 function App() {
   return (
